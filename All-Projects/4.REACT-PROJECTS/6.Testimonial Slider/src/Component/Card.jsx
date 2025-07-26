c802c06de4892251f51f676b837fbee7fdf02638
Your Name <yuvrajvaishnav86@gmail.com>
2025-07-27 03:01:36 +0530

    Updated files for testimonial slider

--- All-Projects/4.REACT-PROJECTS/6.Testimonial Slider/src/Component/Card.jsx
+++ All-Projects/4.REACT-PROJECTS/6.Testimonial Slider/src/Component/Card.jsx
@@ -6,7 +6,7 @@ const Card = (props) => {
     let reviews = props.review;
 
 
-       if (!reviews) {
+    if (!reviews) {
         return <div className="text-center text-red-500">No review data found.</div>;
     }
 
@@ -14,14 +14,14 @@ const Card = (props) => {
     return (
         <div className="flex flex-col md:relative">
 
-            <div className='absolute top-[-7rem] z-[10] mx-auto'>
-                <img
-                    className="aspect-square rounded-full w-[140px] h-[140px] z-[35] className='absolute top-[-7rem] z-[10] mx-auto'"
-                    src={reviews.image}></img>
-                <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px]
-                z-[-10] left-[10px]"></div>
-            </div>
-
+            <img
+                src={reviews.image}
+                className="aspect-square rounded-full w-[120px] h-[120px] md:w-[140px] md:h-[140px] z-[35] mx-auto"
+            />
+            <div
+                className="w-[120px] h-[120px] md:w-[140px] md:h-[140px] bg-violet-500 rounded-full absolute top-[-6px]
+  z-[-10] left-[10px] mx-auto"
+            />
 
             <div className="text-center mt-7">
                 <p className="font-bold tracking-wider text-2xl capitalize">{reviews.name}</p>
